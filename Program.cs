e01b9f1d7975f6884ec43afe5175514dc5b72652
hrmedeiros <hudson.medeiros@haztec.com.br>
2020-04-21 16:44:18 -0300

    Hello World

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Konsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
